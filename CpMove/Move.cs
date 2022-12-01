7f7a4a118501c54ed7e665548a87b59cb02493c1
Laptop <Laptop@DESKTOP-T33BF5Q>
2022-12-01 11:55:24 +0200

    Add project files.

---
+++ CpMove/Move.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpMove
+{
+    public class Move
+    {
+        public string PlayerName { get; set; }
+        public int[] Field { get; set; }
+    }
+}
