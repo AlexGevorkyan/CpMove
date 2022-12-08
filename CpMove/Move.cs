0c677255e20e20dc252f6026fc19da29df8edc5d
AlexGevorkyan <gag63a@gmail.com>
2022-12-08 09:08:46 +0200

    Added Serializable attribute

--- CpMove/Move.cs
+++ CpMove/Move.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace CpMove
 {
+    [Serializable]
     public class Move
     {
         public string PlayerName { get; set; }
         public int[] Field { get; set; }
     }
 }
+
+//0 -   empty cell
+//1 -   0
+//2 -   x
